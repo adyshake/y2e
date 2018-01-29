2fdb3c187a461f18077456df7478b19fc1e08f65
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2018-01-29 11:27:26 +0530

    Loads video in a centered non-fullscreened div

--- background.js
+++ background.js
@@ -8,25 +8,3 @@ chrome.browserAction.onClicked.addListener(function(tab)
       chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
     });
 });
-
-chrome.runtime.onMessage.addListener
-(
-    function(request, sender, sendResponse)
-    {
-        if( request.message === "open_new_tab" )
-        {
-            var watch_id_path =  request.url.split("/")[3]
-            var watch_id
-            if(watch_id_path.length > 19)
-            {
-                watch_id = watch_id_path.slice(8, 19)
-            }
-            else
-            {
-                watch_id = watch_id_path.slice(8)
-            }
-            var final_url = "https://www.youtube.com/embed/"+watch_id
-            chrome.tabs.update({"url": final_url});
-        }
-    }
-);
--- content.js
+++ content.js
@@ -6,8 +6,28 @@ chrome.runtime.onMessage.addListener
     {
         if( request.message === "clicked_browser_action" )
         {
+            $( "title").text("YouTube")
+            $( "body" ).empty()
+            $( "body" ).append("<br><br><br><br>")
+            $( "body" ).append("<div align='center'></div>")
+            $( "div" ).append("<iframe title='Youtube Video Player' width='640' height='390' frameborder='0' allowfullscreen style='border: 1px solid black'></iframe>");
+            
             var url = window.location.href;
-            chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
-        }
+            var watch_id_path =  url.split("/")[3]
+            var watch_id
+            if(watch_id_path.length > 19)
+            {
+                watch_id = watch_id_path.slice(8, 19)
+            }
+            else
+            {
+                watch_id = watch_id_path.slice(8)
+            }
+            var final_url = "https://www.youtube.com/embed/"+watch_id
+            
+            $( "iframe").attr({
+                'src': final_url,
+            });
+        }  
     }
 );
